f0fdb2ea7588d7eedc7727111ff39b54bc584e3a
Zach Burkhart <zachburkhart216@gmail.com>
2022-06-07 23:13:19 -0400

    added generate webpage functionality

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-console.log(profileDataArgs);
+const generatePage = require('./src/page-template.js')
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-};
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
